2371c34deca449526470f6841c99295be53da336
andreevsergey <andreev,sv89@yandex.ru>
2022-06-07 16:41:28 +0300

    add sign_in sign_up logout in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :questions
   resources :tests
   resources :categories
+  resources :users, only: [:index]
   #root to: "categories@index"
   root 'categories#index'
 
