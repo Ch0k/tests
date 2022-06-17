83ca1709146c9b960818c0ee89fb4c8a509f2542
andreevsergey <andreev,sv89@yandex.ru>
2022-06-18 00:10:30 +0300

    add migration roles and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_131529) do
+ActiveRecord::Schema.define(version: 2022_06_14_211924) do
 
   create_table "answers", force: :cascade do |t|
     t.string "answer", null: false
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2022_06_07_131529) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -66,6 +77,14 @@ ActiveRecord::Schema.define(version: 2022_06_07_131529) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+categories = Category.create!([{ title: 'DevOps' },
+                               { title: 'Frontend' }])
+tests = Test.create!([{ title: 'Docker', category_id: categories[0].id },
+                      { title: 'Kubernetes', category_id: categories[0].id },
+                      { title: 'HTML', category_id: categories[1].id }])
+questions = Question.create!([{ body: 'What is it Docker?', test_id: tests[0].id },
+                              { body: 'How docker run?', test_id: tests[0].id },
+                              { body: 'Where docker save volumes?', test_id: tests[0].id }])
+answers = Answer.create!([{ question_id: questions[0].id,
+                            answer: 'Docker is an open platform for developing, shipping, 
+  and running applications',
+                            correct: true },
+                          { question_id: questions[0].id,
+                            answer: 'Docker is a tool that was developed to help 
+  define and share multi-container applications',
+                            correct: false },
+                          { question_id: questions[1].id,
+                            answer: 'docker run',
+                            correct: true },
+                          { question_id: questions[1].id,
+                            answer: 'docker start',
+                            correct: false },
+                          { question_id: questions[2].id,
+                            answer: 'Volumes are stored in a part of the host filesystem
+   which is managed by Docker( /var/lib/docker/volumes/ on Linux).',
+                            correct: true },
+                          { question_id: questions[2].id,
+                            answer: 'Docker stores its on-disk time series data under 
+  the directory specified by the flag storage. local. path .
+  The default path is ./data (relative to the working directory), which is good to try 
+  something out quickly but most likely not what you want for actual operations.',
+                            correct: false }])
